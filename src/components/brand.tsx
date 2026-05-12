d8cd48366c2723ea11743f8df0f9af0017600259
LDrivingAcademy <info@ldrivingacademy.co.uk>
2026-05-12 23:30:09 +0100

    Make header logo clearer

--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Brand() {
   return (
-    <Link href="/" className="grid h-14 w-36 place-items-center overflow-hidden rounded-sm bg-black focus-ring sm:h-16 sm:w-44" aria-label="L Driving Academy home">
+    <Link href="/" className="grid h-14 w-40 place-items-center overflow-hidden rounded bg-white p-1 shadow-sm ring-1 ring-white/30 focus-ring sm:h-16 sm:w-48" aria-label="L Driving Academy home">
       <img src="/lda-logo.jpg" alt="L Driving Academy" className="h-full w-full object-contain" />
     </Link>
   );
