3eb9f3ad6e05a4e0e3fb1bfd4712873b99b6f965
LDrivingAcademy <info@ldrivingacademy.co.uk>
2026-05-14 23:36:42 +0100

    Improve header logo display size

--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export function Brand() {
   return (
-    <Link href="/" className="grid h-11 w-32 shrink-0 place-items-center overflow-hidden rounded bg-black shadow-sm ring-1 ring-white/20 focus-ring sm:h-12 sm:w-36" aria-label="L Driving Academy home">
-      <img src="/lda-logo.jpg" alt="L Driving Academy" className="h-full w-full object-contain" />
+    <Link href="/" className="grid h-12 w-40 shrink-0 place-items-center overflow-hidden rounded bg-black shadow-sm ring-1 ring-white/20 focus-ring sm:h-14 sm:w-44" aria-label="L Driving Academy home">
+      <img src="/lda-logo.jpg" alt="L Driving Academy" className="h-full w-full object-cover object-center" />
     </Link>
   );
 }
