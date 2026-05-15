442454e2b03b961f013586b0251826aac89a7116
LDrivingAcademy <info@ldrivingacademy.co.uk>
2026-05-16 00:50:41 +0100

    Fit LDA logo cleanly in header

--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export function Brand() {
   return (
-    <Link href="/" className="grid h-12 w-40 shrink-0 place-items-center overflow-hidden rounded bg-black shadow-sm ring-1 ring-white/20 focus-ring sm:h-14 sm:w-44" aria-label="L Driving Academy home">
-      <img src="/lda-logo.jpg" alt="L Driving Academy" className="h-full w-full object-cover object-center" />
+    <Link href="/" className="grid h-[60px] w-[104px] shrink-0 place-items-center overflow-hidden rounded bg-black p-1 shadow-sm ring-1 ring-white/20 focus-ring sm:h-[75px] sm:w-[130px]" aria-label="L Driving Academy home">
+      <img src="/lda-logo.jpg" alt="L Driving Academy" width={520} height={300} className="h-full w-full object-contain object-center" />
     </Link>
   );
 }
