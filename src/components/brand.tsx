aaeaa70c13ce259a559172acdb6c3f7335a5d352
Josh MN <josh@Joshs-MacBook-Pro.local>
2026-05-19 04:21:11 +0100

    Use 520 by 300 LDA logo asset

--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -15,10 +15,10 @@ export function Brand({ size = "page" }: { size?: BrandSize }) {
       aria-label="L Driving Academy home"
     >
       <img
-        src="/lda-logo-transparent.svg"
+        src="/lda-logo-520.svg"
         alt="L Driving Academy"
-        width={4096}
-        height={2363}
+        width={520}
+        height={300}
         className="h-full w-full object-contain object-left"
       />
     </Link>
