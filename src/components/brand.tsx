2581336b53c8666f42616d022c51b14290ab22e4
LDrivingAcademy <info@ldrivingacademy.co.uk>
2026-06-04 17:10:52 +0100

    Tighten logo sizing for mobile headers

--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 type BrandSize = "home" | "page";
 
 const brandSizes: Record<BrandSize, string> = {
-  home: "h-[62px] w-[108px] sm:h-[72px] sm:w-[125px] lg:h-[78px] lg:w-[135px]",
-  page: "h-[70px] w-[121px] sm:h-[82px] sm:w-[142px] lg:h-[88px] lg:w-[153px]"
+  home: "h-[52px] w-[90px] sm:h-[72px] sm:w-[125px] lg:h-[78px] lg:w-[135px]",
+  page: "h-[54px] w-[94px] sm:h-[82px] sm:w-[142px] lg:h-[88px] lg:w-[153px]"
 };
 
 export function Brand({ size = "page" }: { size?: BrandSize }) {
