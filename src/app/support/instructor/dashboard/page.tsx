ee74bd107af38bb980429a2eacaebf8b2d31ef86
LDrivingAcademy <info@ldrivingacademy.co.uk>
2026-06-04 16:06:09 +0100

    Split instructor support dashboard entry

---
+++ src/app/support/instructor/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import { InstructorSupportContent } from "@/components/instructor-support-content";
+
+export default function DashboardInstructorSupportPage() {
+  return <InstructorSupportContent entry="dashboard" />;
+}
