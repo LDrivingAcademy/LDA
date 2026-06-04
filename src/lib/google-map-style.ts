65b1cfdcc6b27b4928bb3a3bfcd77f95e974d04a
LDrivingAcademy <info@ldrivingacademy.co.uk>
2026-06-04 18:06:00 +0100

    Add clean Google map styling

---
+++ src/lib/google-map-style.ts
@@ -0,0 +1,26 @@
+export const LDA_GOOGLE_MAP_STYLES = [
+  {
+    featureType: "administrative",
+    elementType: "geometry.stroke",
+    stylers: [{ visibility: "off" }]
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "geometry.stroke",
+    stylers: [{ visibility: "off" }]
+  },
+  {
+    featureType: "administrative.province",
+    elementType: "geometry.stroke",
+    stylers: [{ visibility: "off" }]
+  },
+  {
+    featureType: "poi.business",
+    stylers: [{ visibility: "off" }]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#62c9d8" }]
+  }
+];
